4d95d903a8001693827b27a0151b3c53f6b7f896
Ryan Wilcox <rwilcox@wilcoxd.com>
2020-08-06 00:47:09 -0400

    new function for note

--- racket/scribble-utils.rkt
+++ racket/scribble-utils.rkt
@@ -3,14 +3,35 @@
 (require threading) ; package threading-lib
 (require racket/format)
 
-(provide quote-highlight)
+(provide quote-highlight quote-note)
+
 (define (short-str-join s)
   (string-join s ""))
 
 (define (add-attribution strs-as-lists title is-book? author page-num url )
   (append strs-as-lists
-          (~a "\n> \n" "> From " title " by " author " on page " (number->string page-num)  " (" url ")" )
+          (~a "\n> \n" "> - From " title " by " author " on page " (number->string page-num)  " (" url ")" )
            ))
+
+
+(define (add-highlight-and-info strs-as-list title is-book? author page-num url original)
+  (list
+          "\n> " original "\n> - From " title " by " author " on page " (number->string page-num) " (" url ")"
+          "\n\n"
+          "My thoughts: "
+          strs-as-list))
+
+
+(define (quote-note #:title title
+                          #:is-book? [is-book? 'f]
+                          #:author author
+                          #:page-number page-num
+                          #:url [url ""]
+                          #:original-highlight [highlight ""]
+                          . body) (
+~> (add-highlight-and-info body title is-book? author page-num url highlight)
+   (flatten)
+   (short-str-join)))
 
 
 (define (quote-highlight #:title title
